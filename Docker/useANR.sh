6b8a61c1af887a4e2d7492a821d7d589c8ac36cc
westibule <westibule@protonmail.com>
2021-08-10 23:42:27 +0200

    Typo corrected

--- Docker/useANR.sh
+++ Docker/useANR.sh
@@ -1,1 +1,1 @@
-docker run -it --rm --gpus "device=1" -v ~/Github/ANR/:/tf/ANR/ ll/anr python ./DVRUP/Usage/ANR.py ./ANR/input/$1 
+docker run -it --rm --gpus "device=1" -v ~/Github/ANR/:/tf/ANR/ ll/anr python ./ANR/Usage/ANR.py ./ANR/input/$1 
