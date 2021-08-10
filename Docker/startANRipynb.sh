fb8068a67ca0cca2cffa12d286c40b01be65c92e
luterlassus <westibule@protonmail.com>
2021-08-11 00:55:24 +0200

    New directory name implemented

--- Docker/startANRipynb.sh
+++ Docker/startANRipynb.sh
@@ -1,1 +1,1 @@
-docker run -p 8888:8888 -it --rm --gpus "device=1" -v ~/Github/ANR:/tf/ANR/ -v /mnt/extra/Datasets/:/tf/Datasets/ ll/anr 
+docker run -p 8888:8888 -it --rm --gpus "device=1" -v ~/Github/AnalogNoiseReduction:/tf/ANR/ -v /mnt/extra/Datasets/:/tf/Datasets/ ll/anr 
